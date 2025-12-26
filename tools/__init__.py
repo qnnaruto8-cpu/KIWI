4f8625419165b4d0bf50d8122e0d000ae3d629ac
SUDEEPBOT <vibechill978@gmail.com>
2025-12-26 09:49:04 +0530

    Update __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,1 +1,25 @@
+# Database functions expose karo
+from .database import (
+    add_active_chat,
+    remove_active_chat,
+    is_active_chat,
+    get_active_chats,
+)
+
+# Queue functions expose karo
+from .queue import (
+    put_queue,
+    pop_queue,
+    clear_queue,
+    get_queue,
+)
+
+# Call Client (Assistant) expose karo
+from .call import MUSIC_CALL
+
+# Stream logic expose karo
+from .stream import (
+    play_stream,
+    stop_stream,
+)
 
