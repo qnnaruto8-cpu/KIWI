c80d945cc13920a1009655470b7d76c0ebd31b7e
qnnaruto8-cpu <qnnaruto8@gmail.com>
2026-01-03 12:55:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -355,7 +355,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             elif f"@{bot_username}" in text_lower:
                 should_reply = True
             # 3. Name Call (Aniya)
-            elif "aniya" in text_lower:
+            elif "shinju" in text_lower:
                 should_reply = True
         
         # Agar upar wali koi condition match nahi hui, to RETURN (Chup raho)
